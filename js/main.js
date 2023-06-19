b16c8651ac003df4b1aaed6024eba04dfd31cca6
Alexander-SJT <alexander3post@gmail.com>
2023-06-19 20:55:54 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,1 @@
+import burgeMenu from"./burger.js";import map from"./map.js";import accordion from"./accordion.js";import select from"./custom-select.js";import swiper from"./slider.js";burgeMenu(),accordion(),map(),select();
